4a3ebfdcebfb239e77d58947fd5a5559ed5a85d1
Florian Klampfer <mail@qwtel.com>
2020-12-08 21:23:26 +0700

    Fix html response

--- src/html-response.ts
+++ src/html-response.ts
@@ -2,24 +2,17 @@ import { asyncIterableToStream } from 'whatwg-stream-to-async-iter';
 import { aMap } from './iter';
 import { HTML } from './html';
 
-class TextEncoderStreamHTMLResponse extends Response {
+export class HTMLResponse extends Response {
   constructor(html: HTML, init?: ResponseInit) {
-    super(asyncIterableToStream(html).pipeThrough(new TextEncoderStream()), init);
+    if (typeof TextEncoderStream !== 'undefined') {
+      super(asyncIterableToStream(html).pipeThrough(new TextEncoderStream()), init);
+    } else {
+      const encoder = new TextEncoder();
+      const htmlGenerator = aMap((str: string) => encoder.encode(str))(html);
+      super(asyncIterableToStream(htmlGenerator), init);
+    }
     this.headers.set('Content-Type', 'text/html;charset=UTF-8');
   }
 }
 
-export class MapTextEncodeHTMLResponse extends Response {
-  constructor(html: HTML, init?: ResponseInit) {
-    const encoder = new TextEncoder();
-    const htmlGenerator = aMap((str: string) => encoder.encode(str))(html);
-    super(asyncIterableToStream(htmlGenerator), init);
-    this.headers.set('Content-Type', 'text/html;charset=UTF-8');
-  }
-}
-
-export const HTMLResponse = typeof TextEncoderStream !== 'undefined'
-  ? TextEncoderStreamHTMLResponse
-  : MapTextEncodeHTMLResponse;
-
-export { MapTextEncodeHTMLResponse as CFWorkersHTMLResponse }
+export { HTMLResponse as CFWorkersHTMLResponse }
