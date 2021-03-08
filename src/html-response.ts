3f3f108077bfec83c62ee5aee633f2c49e78457a
Florian Klampfer <mail@qwtel.com>
2021-03-08 11:27:47 +0700

    Include text encoder stream polyfill

--- src/html-response.ts
+++ src/html-response.ts
@@ -1,22 +1,14 @@
+import '../vendor/text-encode-transform.js'
 import { asyncIterableToStream } from 'whatwg-stream-to-async-iter';
-import { aMap, aJoin, promiseToAsyncIterable } from './iter';
+
+import { aJoin, promiseToAsyncIterable } from './iter';
 import { HTML } from './html';
 
 export class HTMLResponse extends Response {
   static contentType = 'text/html;charset=UTF-8';
 
   constructor(html: HTML, init?: ResponseInit) {
-    if (typeof TextEncoderStream !== 'undefined') {
-      super(asyncIterableToStream(html).pipeThrough(new TextEncoderStream()), init);
-    } else {
-      // If `TextEncoderStream` is not available, we can use an asynchronous map 
-      // to pass strings to a text encoder on demand.
-      const encoder = new TextEncoder();
-      const textEncoderGenerator = aMap((str: string) => encoder.encode(str));
-      super(asyncIterableToStream(textEncoderGenerator(html)), init);
-    }
-    // Since this class is for HTML responses only, and `TextEncoder` only supports UTF-8, 
-    // we can set this header reliably:
+    super(asyncIterableToStream(html).pipeThrough(new TextEncoderStream()), init);
     this.headers.set('Content-Type', HTMLResponse.contentType);
   }
 }
